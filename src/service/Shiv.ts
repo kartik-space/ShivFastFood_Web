c320354db5b6b40ab3cad3500e0aa7b5d1276b97
Yogesh Vashisth <writetokhair@gmail.com>
2024-08-01 13:07:47 +0530

    free

--- src/service/Shiv.ts
+++ src/service/Shiv.ts
@@ -1,27 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create an Axios instance
 const apiClient = axios.create({
-  baseURL: 'https://shiv-fast-food-backend-wuq9.onrender.com/api/v1',
+  baseURL: "https://shiv-fast-food-backend-wuq9.onrender.com/api/v1",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Fetch menu items
 export const fetchItems = async () => {
-  const response = await apiClient.get('/menu/get-item');
+  const response = await apiClient.get("/menu/get-item");
   return response.data;
 };
 
 // Fetch kitchen status
 export const fetchKitchenStatus = async () => {
-  const response = await apiClient.get('/kitchen/');
+  const response = await apiClient.get("/kitchen/");
   return response.data;
 };
 
+export const placeOrder = async (orderData: any) => {
+  const response = await apiClient.post("/order", orderData);
+  return response.data;
+};
 
-export const placeOrder = async (orderData : any) => {
-  const response = await apiClient.post('/order', orderData);
+export const register = async (uid: any) => {
+  const response = await apiClient.post("/user/register", { uid });
   return response.data;
 };
